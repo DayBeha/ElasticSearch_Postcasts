de9c266bbdb8850519ef4f7a56c6b33fe31a2375
BillXu0424 <1065602877@qq.com>
2023-04-12 20:57:21 +0200

    Adjust the new score of the transcript.

--- transcript_dicts.py
+++ transcript_dicts.py
@@ -1,6 +1,7 @@
 from transcript import Transcript
 from functools import cmp_to_key
 from typing import Dict, List
+from copy import deepcopy
 
 class TranscriptDicts():
     def __init__(self) -> None:
@@ -14,17 +15,20 @@ class TranscriptDicts():
             transcript (Transcript): The relevant transcript to be inserted.
             episode_filename_prefix (str): Identifier for the episode.
             base_score (float): Weighted score of episode and show.
-            retrieved_trans (List[Dict]): All transcripts for the episode, sorted chronologically. How to: when use episode_filename_prefix to search, first get total number of transcripts in that episode by res['hits']['total']['value'], then search again and set size=total and you can get all the transcripts in this episode.
+            retrieved_trans (List[Dict]): All transcripts for the episode, sorted chronologically. How to: when use episode_filename_prefix to search, first get total number of transcripts in that episode by res['hits']['total']['value'], then search again and set size = res['hits']['total']['value'] and you can get all the transcripts in this episode.
         """
-    
+
+        transcript = deepcopy(transcript)
+        transcript.set_score(transcript.get_score() * base_score) # maybe a weight should be applied...
+        
         if episode_filename_prefix not in self.transcript_dicts:
             self.transcript_dicts[episode_filename_prefix] = list()
-            for trans in retrieved_trans:
-                trans_obj = Transcript(json_obj=trans)
+            for trans_json in retrieved_trans:
+                trans_obj = Transcript(json_obj=trans_json)
                 if trans_obj.get_id() == transcript.get_id():
                     self.transcript_dicts[episode_filename_prefix].append(transcript)
                     continue
-                trans_obj.set_score(base_score)
+                trans_obj.set_score(0)
                 self.transcript_dicts[episode_filename_prefix].append(trans_obj)
         else:
             self.transcript_dicts[episode_filename_prefix][transcript.get_id()] = transcript
